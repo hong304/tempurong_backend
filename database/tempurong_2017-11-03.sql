d89d24e82a6803b6f792d72c885344b8c9782669
cleochan <cc@buildonauts.com>
2017-11-03 18:48:44 +0800

    add database sql

---
+++ database/tempurong_2017-11-03.sql
@@ -0,0 +1,177 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.18-0ubuntu0.16.04.1)
+# Database: tempurong
+# Generation Time: 2017-11-03 10:48:05 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table admin
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table reservation
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `reservation`;
+
+CREATE TABLE `reservation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `transaction_id` varchar(255) DEFAULT NULL,
+  `payment_method` varchar(255) DEFAULT NULL,
+  `client_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table room_reservation
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `room_reservation`;
+
+CREATE TABLE `room_reservation` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) DEFAULT NULL,
+  `room_type_id` int(11) DEFAULT NULL,
+  `reservation_id` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `no_of_people` int(11) DEFAULT NULL,
+  `add_bed` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `refund_status` varchar(255) DEFAULT NULL,
+  `start_date` timestamp NULL DEFAULT NULL,
+  `end_date` timestamp NULL DEFAULT NULL,
+  `status_time` timestamp NULL DEFAULT NULL,
+  `refund_time` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table rooms
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `rooms`;
+
+CREATE TABLE `rooms` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+
+INSERT INTO `rooms` (`id`, `type_id`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	(1,1,NULL,NULL,NULL),
+	(2,1,NULL,NULL,NULL),
+	(3,1,NULL,NULL,NULL),
+	(4,1,NULL,NULL,NULL),
+	(5,1,NULL,NULL,NULL),
+	(6,1,NULL,NULL,NULL),
+	(7,1,NULL,NULL,NULL),
+	(8,1,NULL,NULL,NULL),
+	(9,2,NULL,NULL,NULL),
+	(10,2,NULL,NULL,NULL),
+	(11,2,NULL,NULL,NULL),
+	(12,3,NULL,NULL,NULL),
+	(13,3,NULL,NULL,NULL);
+
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table room_images
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `room_images`;
+
+CREATE TABLE `room_images` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `room_id` int(11) DEFAULT NULL,
+  `cover_photo` tinyint(1) DEFAULT NULL,
+  `path` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table room_types
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `room_types`;
+
+CREATE TABLE `room_types` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `capacity` int(10) DEFAULT NULL,
+  `add_bed` tinyint(1) DEFAULT '0',
+  `price` int(10) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `room_types` WRITE;
+/*!40000 ALTER TABLE `room_types` DISABLE KEYS */;
+
+INSERT INTO `room_types` (`id`, `name`, `capacity`, `add_bed`, `price`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	(1,'sea view',4,1,NULL,NULL,NULL,NULL),
+	(2,'river view (big)',6,0,NULL,NULL,NULL,NULL),
+	(3,'river view (small)',4,1,NULL,NULL,NULL,NULL);
+
+/*!40000 ALTER TABLE `room_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
