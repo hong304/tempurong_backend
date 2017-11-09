8179049e97bc37cdd569ed0bbf982a71c0e57836
cleochan <cc@buildonauts.com>
2017-11-09 18:50:39 +0800

    update database file

--- database/tempurong_2017-11-09.sql
+++ database/tempurong_2017-11-09.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.18-0ubuntu0.16.04.1)
 # Database: tempurong
-# Generation Time: 2017-11-07 10:44:10 +0000
+# Generation Time: 2017-11-09 10:50:07 +0000
 # ************************************************************
 
 
@@ -20,19 +20,42 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-# Dump of table users
+# Dump of table reservation_details
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `reservation_details`;
 
-CREATE TABLE `users` (
+CREATE TABLE `reservation_details` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  `room_type_id` int(11) DEFAULT NULL,
+  `reservation_id` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `no_of_people` int(11) DEFAULT NULL,
+  `add_bed` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `refund_status` varchar(255) DEFAULT NULL,
+  `start_date` timestamp NULL DEFAULT NULL,
+  `end_date` timestamp NULL DEFAULT NULL,
+  `status_time` timestamp NULL DEFAULT NULL,
+  `refund_time` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+LOCK TABLES `reservation_details` WRITE;
+/*!40000 ALTER TABLE `reservation_details` DISABLE KEYS */;
+
+INSERT INTO `reservation_details` (`id`, `room_id`, `room_type_id`, `reservation_id`, `price`, `capacity`, `no_of_people`, `add_bed`, `status`, `refund_status`, `start_date`, `end_date`, `status_time`, `refund_time`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	(1,1,1,1,250,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+	(2,2,1,1,500,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+/*!40000 ALTER TABLE `reservation_details` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table reservations
@@ -84,44 +107,6 @@ CREATE TABLE `room_images` (
 
 
 
-# Dump of table reservation_details
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `reservation_details`;
-
-CREATE TABLE `reservation_details` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `room_id` int(11) DEFAULT NULL,
-  `room_type_id` int(11) DEFAULT NULL,
-  `reservation_id` int(11) DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  `capacity` int(11) DEFAULT NULL,
-  `no_of_people` int(11) DEFAULT NULL,
-  `add_bed` int(11) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
-  `refund_status` varchar(255) DEFAULT NULL,
-  `start_date` timestamp NULL DEFAULT NULL,
-  `end_date` timestamp NULL DEFAULT NULL,
-  `status_time` timestamp NULL DEFAULT NULL,
-  `refund_time` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-LOCK TABLES `reservation_details` WRITE;
-/*!40000 ALTER TABLE `reservation_details` DISABLE KEYS */;
-
-INSERT INTO `reservation_details` (`id`, `room_id`, `room_type_id`, `reservation_id`, `price`, `capacity`, `no_of_people`, `add_bed`, `status`, `refund_status`, `start_date`, `end_date`, `status_time`, `refund_time`, `created_at`, `updated_at`, `deleted_at`)
-VALUES
-	(1,1,1,1,250,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
-	(2,2,1,1,500,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-
-/*!40000 ALTER TABLE `reservation_details` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
 # Dump of table room_types
 # ------------------------------------------------------------
 
@@ -129,7 +114,8 @@ DROP TABLE IF EXISTS `room_types`;
 
 CREATE TABLE `room_types` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `name_en` varchar(255) DEFAULT NULL,
+  `name_sc` varchar(255) DEFAULT NULL,
   `capacity` int(10) DEFAULT NULL,
   `add_bed` tinyint(1) DEFAULT '0',
   `price` int(10) DEFAULT NULL,
@@ -142,11 +128,11 @@ CREATE TABLE `room_types` (
 LOCK TABLES `room_types` WRITE;
 /*!40000 ALTER TABLE `room_types` DISABLE KEYS */;
 
-INSERT INTO `room_types` (`id`, `name`, `capacity`, `add_bed`, `price`, `created_at`, `updated_at`, `deleted_at`)
+INSERT INTO `room_types` (`id`, `name_en`, `name_sc`, `capacity`, `add_bed`, `price`, `created_at`, `updated_at`, `deleted_at`)
 VALUES
-	(1,'sea view',4,1,250,NULL,NULL,NULL),
-	(2,'river view (big)',6,0,274,NULL,NULL,NULL),
-	(3,'river view (small)',4,1,250,NULL,NULL,NULL);
+	(1,'sea view','海景房',4,1,250,NULL,NULL,NULL),
+	(2,'river view (big)','河景房（大）',6,0,274,NULL,NULL,NULL),
+	(3,'river view (small)','河景房（小）',4,1,250,NULL,NULL,NULL);
 
 /*!40000 ALTER TABLE `room_types` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -187,6 +173,21 @@ VALUES
 
 /*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 
