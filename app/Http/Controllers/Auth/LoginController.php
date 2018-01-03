11dded9aff71e007b7e2d87676b8ff0db5a4a952
key60529 <ppkayunk@gmail.com>
2018-01-03 15:22:26 +0800

    try authen

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Console\Parser;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class LoginController extends Controller
 {
@@ -29,7 +31,6 @@ class LoginController extends Controller
 	 * @var string
 	 */
 	protected $redirectTo = '/';
-  public $successStatus = 200;
 	
 	/**
 	 * Create a new controller instance.
@@ -49,27 +50,63 @@ class LoginController extends Controller
 	{
     if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
       $user = Auth::user();
-      $success['token'] =  $user->createToken($user->name)->accessToken;
-      return response()->json(['success' => $success], $this->successStatus);
+
+      $result = [
+        'status' => true,
+        'message' => 'Login Success',
+        'token' => $user->createToken($user->name)->accessToken
+      ];
+      return response()->json($result, 200);
     }
     else{
-      return response()->json(['error'=>'Login failed.'], 401);
+
+      $result = [
+        'status' => false,
+        'message' => 'Login Failed'
+      ];
+
+      return response()->json($result, 401);
     }
 	}
 	
 	public function checkLogin()
 	{
-    $user = Auth::user();
-    return response()->json(['success' => $user], $this->successStatus);
+	  if (Auth::check()) {
+      $user = Auth::user();
+      $result = [
+        'status' => true,
+        'message' => 'Logged-in.',
+        'user_data' => $user
+      ];
+    } else {
+      $result = [
+        'status' => false,
+        'message' => 'Not Logged-in.'
+      ];
+    }
+    return response()->json($result, 200);
 	}
 	
-	public function logout()
+	public function logout(Request $request)
 	{
-		Auth::logout();
-		$result = [
-			'status' => true,
-			'message' => 'Logged-out.'
-		];
-		return response()->json($result, 200);
+    $value = $request->bearerToken();
+    $id = (new Parser())->parse($value)->getHeader('jti');
+
+    $token = DB::table('oauth_access_tokens')
+      ->where('id', '=', $id)
+      ->update(['revoked' => true]);
+
+    $this->guard()->logout();
+
+    $request->session()->flush();
+
+    $request->session()->regenerate();
+
+    $result = [
+      'status' => true,
+      'message' => 'You are Logged out.'
+    ];
+
+    return response()->json($result, 200);
 	}
 }
