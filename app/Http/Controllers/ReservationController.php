17f15e18ab555110e78c0d36cd994fd42f02294b
cleochan <cc@buildonauts.com>
2018-01-12 19:51:00 +0800

    reservation api bug fix

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -155,6 +155,15 @@ class ReservationController extends Controller
 		}
 	}
 	
+	public function postCheckAvailableRooms(Request $request)
+	{
+		$checkInDate = $request->checkIn;
+		$checkOutDate = $request->checkOut;
+		
+		return $this->getAvailableRoomData($checkInDate, $checkOutDate, $request->withRoomDetails);
+		
+	}
+	
 	public function getAvailableRoomData($checkInDate, $checkOutDate, $withRoomDetails)
 	{
 		$withRoomDetails = $withRoomDetails || false;
@@ -166,7 +175,7 @@ class ReservationController extends Controller
 			foreach ($room_type_ids as $k => $room_type_id) {
 				
 				//reorder reservation in DB of this room type
-				$this->reorderReservation($room_type_id);
+				$this->reorderReservation($room_type_id, $checkInDate, $checkOutDate);
 				
 				// select available room again
 				// put available room value to array
@@ -198,14 +207,128 @@ class ReservationController extends Controller
 		
 	}
 	
-	public function postCheckAvailableRooms(Request $request)
+	public function reorderReservation($roomTypeId, $checkInDate, $checkOutDate)
 	{
-		$checkInDate = $request->checkIn;
-		$checkOutDate = $request->checkOut;
+		$today = date('Y-m-d');
 		
-		return $this->getAvailableRoomData($checkInDate, $checkOutDate, $request->withRoomDetails);
+		// select all rooms and count no of rooms of that room type
+		$count_rooms = Room::where('room_type_id', $roomTypeId)->pluck('id');
 		
+		// select all room orders that cannot be changed
+		$fixed_block = ReservationDetails::where('start_date', '<=', $today)
+			->where('end_date', '>=', $today)
+			->where('room_type_id', $roomTypeId)
+			->where('status', '!=', 'refunded')
+			->get();
+		
+		$new_block = new ReservationDetails();
+		$new_block->start_date = $checkInDate;
+		$new_block->end_date = $checkOutDate;
+		$new_block->room_type_id = $roomTypeId;
+		$new_block->status = 'temp';
+		$new_block->save();
+		
+		// select all other room orders that after today and order by start date
+		$movement_block = ReservationDetails::where('start_date', '>=', $today)
+			->where('room_type_id', $roomTypeId)
+			->where('status', '!=', 'refunded')
+			->orderBy('start_date', 'asc')
+			->orderBy('end_date', 'asc')
+			->get();
+		
+		// define an empty room array
+		$rooms = [];
+		
+		// add start date of each room into the empty room array
+		for ($i = 0; $i < count($count_rooms); $i++) {
+			$rooms[$i]['id'] = $count_rooms[$i];
+			$rooms[$i]['end_date'] = $today;
+			foreach ($fixed_block as $key => $v) {
+				// if there is order cannot be changed,
+				// add the end date to room array as start date
+				if ($v->room_id == $count_rooms[$i]) {
+					$rooms[$i]['end_date'] = $v->end_date;
+					break;
+				}
+			}
+			
+		}
+		
+		// loop room array
+		foreach ($rooms as &$room) {
+			// loop room orders
+			foreach ($movement_block as $key => $block) {
+				// if the start date of room order >= room's start date
+				// means the room order is the nearest next order of that room
+				if ($block->start_date >= $room['end_date']) {
+					// update room array's start date
+					$room['end_date'] = $block->end_date;
+					// change the order room id to current room id
+					$block->room_id = $room['id'];
+					// then add the room order to the "order cannot be changed" array
+					$fixed_block->push($block);
+					// and forget the one in room order array
+					$movement_block->forget($key);
+				}
+				
+			}
+		}
+		
+		// update room order in database
+		if ($movement_block->count() == 0) {
+			foreach ($fixed_block as $v) {
+				$reservationDetails = ReservationDetails::where('id', $v->id)->first();
+				$reservationDetails->room_id = $v->room_id;
+				$reservationDetails->save();
+			}
+		}
+		$new_block->forceDelete();
 	}
+	
+	public function getAvailableRoomNumber($checkInDate, $checkOutDate, $roomTypeId)
+	{
+		// select all rooms and count no of rooms of that room type
+		$total_no_of_rooms = Room::where('room_type_id', $roomTypeId)->count();
+		//13Jan 19Jan
+		$passingCheckIn = ReservationDetails::where(function ($q1) use ($checkInDate, $checkOutDate) {
+			$q1->where(function ($q) use ($checkInDate) {
+				$q->where('start_date', '<=', $checkInDate)->where('end_date', '>', $checkInDate);
+			})->orwhere(function ($q) use ($checkInDate, $checkOutDate) {
+				$q->where('start_date', '>=', $checkInDate)->where('end_date', '<=', $checkOutDate);
+			})->orwhere(function ($q) use ($checkOutDate) {
+				$q->where('start_date', '<', $checkOutDate)->where('end_date', '>=', $checkOutDate);
+			});
+		})->where('status', '!=', 'refunded')
+			->where('room_type_id', $roomTypeId)->distinct()->get(['room_id'])->count();
+		
+		// count available room
+		$avail_room = $total_no_of_rooms - $passingCheckIn;
+		
+		return $avail_room;
+	}
+	
+	public function getAvailableRoomId($checkInDate, $checkOutDate, $roomTypeId)
+	{
+		$allRoomId = Room::where('room_type_id', $roomTypeId)->get()->pluck('id')->toArray();
+		// select all rooms and count no of rooms of that room type
+		$roomOccupied = ReservationDetails::where(function ($q1) use ($checkInDate, $checkOutDate) {
+			
+			$q1->where(function ($q) use ($checkInDate) {
+				$q->where('start_date', '<=', $checkInDate)->where('end_date', '>', $checkInDate);
+			})->orwhere(function ($q) use ($checkInDate, $checkOutDate) {
+				$q->where('start_date', '>=', $checkInDate)->where('end_date', '<=', $checkOutDate);
+			})->orwhere(function ($q) use ($checkOutDate) {
+				$q->where('start_date', '<', $checkOutDate)->where('end_date', '>=', $checkOutDate);
+			});
+		})->where(function ($query) {
+			$query->where('status', 'waiting_for_payment')
+				->orWhere('status', 'completed');
+		})->where('room_type_id', $roomTypeId)->distinct('room_id')->get()->pluck('room_id')->toArray();
+		
+		$roomAvail = array_values(array_diff($allRoomId, $roomOccupied));
+		return $roomAvail;
+	}
+	
 	
 	public function updateReservationStatus(Request $request)
 	{
@@ -247,117 +370,6 @@ class ReservationController extends Controller
 		
 	}
 	
-	public function testEmail(Request $request)
-	{
-		$reservation = Reservation::where('id', $request->id)->with(['reservationDetails.roomType'])->first();
-		$reservation->total_rooms = count($reservation->reservationDetails);
-		$reservation->total_nights = $this->getTotalNight($reservation->check_in, $reservation->check_out);
-//		dd($reservation);
-		$reservation->notify(new ReservationPaid($reservation));
-	}
-	
-	public function getAvailableRoomNumber($checkInDate, $checkOutDate, $roomTypeId)
-	{
-		// select all rooms and count no of rooms of that room type
-		$total_no_of_rooms = Room::where('room_type_id', $roomTypeId)->count();
-		
-		$passingCheckIn = ReservationDetails::where(function ($q1) use ($checkInDate, $checkOutDate) {
-			
-			$q1->where(function ($q) use ($checkInDate) {
-				$q->where('start_date', '<=', $checkInDate)->where('end_date', '>', $checkInDate);
-			})->orwhere(function ($q) use ($checkInDate, $checkOutDate) {
-				$q->where('start_date', '>=', $checkInDate)->where('end_date', '<=', $checkOutDate);
-			})->orwhere(function ($q) use ($checkOutDate) {
-				$q->where('start_date', '<', $checkOutDate)->where('end_date', '>=', $checkOutDate);
-			});
-		})->where(function ($query) {
-			$query->where('status', 'waiting_for_payment')
-				->orWhere('status', 'completed');
-		})->where('room_type_id', $roomTypeId)->distinct('room_id')->count();
-		
-		// count available room
-		$avail_room = $total_no_of_rooms - $passingCheckIn;
-		
-		return $avail_room;
-	}
-	
-	public function getAvailableRoomId($checkInDate, $checkOutDate, $roomTypeId)
-	{
-		$allRoomId = Room::where('room_type_id', $roomTypeId)->get()->pluck('id')->toArray();
-		// select all rooms and count no of rooms of that room type
-		$roomOccupied = ReservationDetails::where(function ($q1) use ($checkInDate, $checkOutDate) {
-			
-			$q1->where(function ($q) use ($checkInDate) {
-				$q->where('start_date', '<=', $checkInDate)->where('end_date', '>', $checkInDate);
-			})->orwhere(function ($q) use ($checkInDate, $checkOutDate) {
-				$q->where('start_date', '>=', $checkInDate)->where('end_date', '<=', $checkOutDate);
-			})->orwhere(function ($q) use ($checkOutDate) {
-				$q->where('start_date', '<', $checkOutDate)->where('end_date', '>=', $checkOutDate);
-			});
-		})->where(function ($query) {
-			$query->where('status', 'waiting_for_payment')
-				->orWhere('status', 'completed');
-		})->where('room_type_id', $roomTypeId)->distinct('room_id')->get()->pluck('room_id')->toArray();
-		
-		$roomAvail = array_values(array_diff($allRoomId, $roomOccupied));
-		return $roomAvail;
-	}
-	
-	public function reorderReservation($roomTypeId)
-	{
-		
-		$today = date('Y-m-d');
-		
-		// select all rooms and count no of rooms of that room type
-		$count_rooms = Room::select('id')->where('room_type_id', $roomTypeId)->get();
-		
-		// select all room orders that cannot be changed
-		$fixed_block = ReservationDetails::where('start_date', '<', $today)->where('end_date', '>=', $today)->where('room_type_id', $roomTypeId)->get();
-		
-		// select all other room orders that after today and order by start date
-		$movement_block = ReservationDetails::where('start_date', '>=', $today)->orderBy('start_date', 'asc')->orderBy('end_date', 'asc')->get();
-		
-		// define an empty room array
-		$rooms = [];
-		
-		// add start date of each room into the empty room array
-		for ($i = 0; $i < count($count_rooms); $i++) {
-			$rooms[$count_rooms{$i}->id] = '';
-			foreach ($fixed_block as $key => $v) {
-				// if there is order cannot be changed,
-				// add the end date to room array as start date
-				if ($v->room_id == $count_rooms{$i}->id) {
-					$rooms[$count_rooms{$i}->id] = $v->end_date;
-					break;
-				}
-			}
-			
-		}
-		
-		// loop room array
-		foreach ($rooms as $key => &$v) {
-			// loop room orders
-			foreach ($movement_block as $key1 => $v1) {
-				// if the start date of room order >= room's start date
-				// means the room order is the nearest next order of that room
-				if ($v1->start_date >= $v) {
-					// update room array's start date
-					$v = $v1->end_date;
-					// change the order room id to current room id
-					$v1->room_id = $key;
-					// then add the room order to the "order cannot be changed" array
-					$fixed_block->push($v1);
-					// and forget the one in room order array
-					$movement_block->forget($key1);
-				}
-			}
-		}
-		
-		// update room order in database
-		foreach ($fixed_block as $v) {
-			ReservationDetails::where('id', $v->id)->update(['room_id' => $v->room_id]);
-		}
-	}
 	
 	public function deleteInvalidReservation()
 	{
@@ -378,6 +390,15 @@ class ReservationController extends Controller
 		
 		$reservationDetails->delete();
 		
+	}
+	
+	public function testEmail(Request $request)
+	{
+		$reservation = Reservation::where('id', $request->id)->with(['reservationDetails.roomType'])->first();
+		$reservation->total_rooms = count($reservation->reservationDetails);
+		$reservation->total_nights = $this->getTotalNight($reservation->check_in, $reservation->check_out);
+//		dd($reservation);
+		$reservation->notify(new ReservationPaid($reservation));
 	}
 	
 	private function validateRoomObjects($roomObjects)
